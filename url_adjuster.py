c375b9f279222e9c7cbd425e00ca4373f9828372
Dylan <dmcintosh@chapman.edu>
2024-04-17 20:13:18 -0700

    Creates new file to add correctedUrl column to the csv that should correspond to the correct video, need to confirm that everything is working properly

---
+++ url_adjuster.py
@@ -0,0 +1,17 @@
+#%%
+import pandas as pd
+
+# Load csv data
+df = pd.read_csv('video_data.csv')
+
+# Create correctedUrl using format and replace username and id
+df['correctedUrl'] = "https://www.tiktok.com/@" + df['author'].astype(str) + "/video/" + df['id'].astype(str)
+
+print(df.columns)
+
+df.drop(columns=['Unnamed: 0'], inplace=True)
+
+df.head()
+
+df.to_csv('video_data.csv', index=False)
+# %%
